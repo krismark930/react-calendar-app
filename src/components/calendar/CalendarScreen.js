3be23de38b6de7891b57e9a98ad318a269681159
happysmile911003 <happysmile911003@gmail.com>
2021-06-25 10:49:43 -0500

    Integración de React Big Calendar.
    Integración ligera personalización de los colores de los eventos y
    traducción de los mensajes del calendario

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,10 +1,59 @@
 import React from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+
+
 import Navbar from '../ui/Navbar';
+import { messages } from '../../helpers/calendar-messages-es';
+
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import 'moment/locale/es';
+
+moment.locale('es');
+
+// Setup the localizer by providing the moment (or globalize) Object
+// to the correct localizer.
+const localizer = momentLocalizer(moment); // or globalizeLocalizer
+
+const events = [{
+    title: 'Cumpleaños del jefe',
+    start: moment().toDate(),
+    end: moment().add( 2, 'hours').toDate(),
+    bgcolor: '#fafafa'
+}];
 
 const CalendarScreen = () => {
+
+    const eventStyleGetter = ( event, start, end, isSelected ) => {
+        console.log( event, start, end, isSelected );
+
+        const style = {
+            backgroundColor: '#367CF7',
+            borderRadius: '0px',
+            opacity: 0.8,
+            display: 'block',
+            color: 'white'
+        }
+
+        return {
+            style
+        }
+    }
+    
     return (
-        <div>
+        <div className="calendar-screen">
+
             <Navbar />
+
+
+            <Calendar
+                localizer={ localizer }
+                events={ events }
+                startAccessor="start"
+                endAccessor="end"
+                messages={ messages }
+                eventPropGetter={ eventStyleGetter }
+            />
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CalendarApp from './CalendarApp';
 
+import './styles.css';
+
 ReactDOM.render(
     <CalendarApp />,
   document.getElementById('root')
