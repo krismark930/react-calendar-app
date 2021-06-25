0c83b04926917d255e1e87682e7aefc6212c3b68
happysmile911003 <happysmile911003@gmail.com>
2021-06-24 20:42:57 -0500

    LoginScreen y Navbar

---
+++ src/components/calendar/CalendarScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../ui/Navbar';
+
+const CalendarScreen = () => {
+    return (
+        <div>
+            <Navbar />
+        </div>
+    )
+}
+
+export default CalendarScreen
---
+++ src/components/ui/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className="navbar navbar-dark bg-dark mb-4">
+
+            <span className="navbar-brand">
+                Juan
+            </span>
+
+            <button className="btn btn-outline-danger">
+                <i className="fas fa-sign-out-alt"></i>
+                <span> Salir</span>
+            </button>
+            
+        </div>
+    )
+}
+
+export default Navbar
